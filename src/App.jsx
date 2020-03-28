baae7ffd7604a1c6fbb5b2c97a5beecf3fcb6bc0
KamariRoss <kamarimobile@gmail.com>
2020-03-28 17:10:23 -0400

    brought commetted out movies without component

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,8 @@ const Movie = (props) => {
   )
 }
 const App = () => {
+  const movies = []
+
   return (
     <div>
       <header>
@@ -47,6 +49,56 @@ const App = () => {
             against convention and seize the potential of every day,
             courting the disdain of the stern headmaster."
           />
+          {/* <li>
+            {' '}
+            <h3>Indiana Jones and the Last Crusade</h3>
+            <div class="movie-image-and-caption">
+              <img
+                src="https://image.tmdb.org/t/p/w185_and_h278_bestv2//4p1N2Qrt8j0H8xMHMHvtRxv9weZ.jpg"
+                alt="Indiana Jones and the Last Crusade poster"
+              />
+              <p>
+                {' '}
+                When Dr. Henry Jones Sr. suddenly goes missing while pursuing
+                the Holy Grail, eminent archaeologist Indiana must team up with
+                Marcus Brody, Sallah and Elsa Schneider to follow in his
+                father's footsteps and stop the Nazis from recovering the power
+                of eternal life.
+              </p>
+            </div>
+          </li>
+          <li>
+            <h3> The Gods Must Be Crazy II</h3>
+            <div class="movie-image-and-caption">
+              <img
+                src="https://image.tmdb.org/t/p/w185_and_h278_bestv2/zdKMqYiy7s5eyITRf9Q19WtyBxQ.jpg"
+                alt="The Gods Must Be Crazy II poster"
+              />
+              <p>
+                Xixo is back again. This time, his children accidentally stow
+                away on a fast-moving poachers' truck, unable to get off, and
+                Xixo sets out to rescue them. Along the way, he encounters a
+                couple of soldiers trying to capture each other and a pilot and
+                passenger of a small plane, who are each having a few problems
+                of their own.
+              </p>
+            </div>
+          </li>
+          <li>
+            <h3>Dead Poets Society</h3>
+            <div class="movie-image-and-caption">
+              <img
+                src="https://image.tmdb.org/t/p/w185_and_h278_bestv2/3Ri2GReavqSHqWemlP6HYn8i2P9.jpg"
+                alt="Dead Poets Society poster"
+              />
+              <p>
+                At an elite, old-fashioned boarding school in New England, a
+                passionate English teacher inspires his students to rebel
+                against convention and seize the potential of every day,
+                courting the disdain of the stern headmaster.{' '}
+              </p>
+            </div>
+          </li> */}
         </ul>
       </article>
       <footer> Because every year sucks I guess</footer>
