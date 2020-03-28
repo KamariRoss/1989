3013d526e20d9be89a90a0ee61f600da9207734c
KamariRoss <kamarimobile@gmail.com>
2020-03-28 19:22:41 -0400

    explorer mode completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import HelloWorld from './components/HelloWorld'
 const Movie = (props) => {
   const imgUrl = `https://image.tmdb.org/t/p/w185_and_h278_bestv2/${props.poster_path}`
@@ -14,7 +14,21 @@ const Movie = (props) => {
   )
 }
 const App = () => {
-  const movies = []
+  const [movies, setMovies] = useState([])
+
+  useEffect(() => {
+    fetch(
+      'https://api.themoviedb.org/3/discover/movie?primary_release_year=1989&sort_by=popularity.desc&api_key=7a5c745b3d90a7b648e3adef4bad5137'
+    )
+      //returns a promise
+      .then((response) => response.json())
+      //returns another promise
+      .then((apiData) => {
+        const newMovies = apiData.results
+        console.log(newMovies)
+        setMovies(newMovies)
+      })
+  }, [])
 
   return (
     <div>
@@ -26,29 +40,16 @@ const App = () => {
       </header>
       <article>
         <ul class="movies">
-          <Movie
-            title="Indiana Jones and the Last Crusade"
-            poster_path="/4p1N2Qrt8j0H8xMHMHvtRxv9weZ.jpg"
-            overview="When Dr. Henry Jones Sr. suddenly goes missing while pursuing the Holy Grail, eminent archaeologist Indiana must team up with Marcus Brody, Sallah and Elsa Schneider to follow in his father's footsteps and stop the Nazis from recovering the power of eternal life."
-          />
-          <Movie
-            title="The Gods Must Be Crazy II"
-            poster_path="zdKMqYiy7s5eyITRf9Q19WtyBxQ.jpg"
-            overview="Xixo is back again. This time, his children accidentally stow
-            away on a fast-moving poachers' truck, unable to get off, and
-            Xixo sets out to rescue them. Along the way, he encounters a
-            couple of soldiers trying to capture each other and a pilot and
-            passenger of a small plane, who are each having a few problems
-            of their own."
-          />
-          <Movie
-            title="Dead Poets Society"
-            poster_path="3Ri2GReavqSHqWemlP6HYn8i2P9.jpg"
-            overview="At an elite, old-fashioned boarding school in New England, a
-            passionate English teacher inspires his students to rebel
-            against convention and seize the potential of every day,
-            courting the disdain of the stern headmaster."
-          />
+          {movies.map((movie) => {
+            return (
+              <Movie
+                title={movie.title}
+                poster_path={movie.poster_path}
+                overview={movie.overview}
+              />
+            )
+          })}
+
           {/* <li>
             {' '}
             <h3>Indiana Jones and the Last Crusade</h3>
